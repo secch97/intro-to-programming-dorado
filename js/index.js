151c97e3efa82f1e168ca93a1bef3b376be37f22
Saul Castillo <secch97@gmail.com>
2023-01-01 15:33:37 -0500

    Modified media queries and flex display of navigation bars of header and footer

--- js/index.js
+++ js/index.js
@@ -7,13 +7,13 @@ function showCopyright() {
   //2. Retrieve the current year:
   let thisYear = today.getFullYear();
   //3. Selecting footerNav from my DOM:
-  const footerNav = document.querySelector("footer #footerNav");
+  const footerNav = document.querySelector(".footerNav");
   //4. Create a new paragraph element:
   const copyright = document.createElement("p");
   //5. Add a class to the new paragraph element
   copyright.classList.add("footer-element");
   //6. Write my name and current year to the copyright paragraph element:
-  copyright.innerHTML = `© Saul Castillo | ${thisYear}`;
+  copyright.innerText = `© Saul Castillo | ${thisYear}`;
   //7. Append the copyright paragraph into the footerNav element.
   footerNav.appendChild(copyright);
 }
